b6ef79eef51e83da2521b445d43cd2f2325e7bc6
Srinidhi S Prasad <64426998+Srin01@users.noreply.github.com>
2022-10-12 15:06:02 +0530

    Initial Commit

---
+++ login.py
@@ -0,0 +1,14 @@
+#Ganesha
+# https://api.icicidirect.com/apiuser/login?api_key=$8G30507bD0652J17787414K4r83c)ay
+# https://api.icicidirect.com/apiuser/192.168.9.6
+
+# https://api.icicidirect.com/apiuser/login?api_key= 7333Z410qM085d3289z3LrDO2`87435#
+# https://api.icicidirect.com/apiuser/login?api_key=J6188V=q37e&859165530%226P9632ov
+
+
+# username=''
+# password=''
+session_key='1762788'
+api_key='77u105178j2!70(6gj01w9^452R1S3n9'
+api_secret= 'p333)331Uj587#J7o*6E4f656&u201v0'
+current_expiry_dt = "2022-10-13T06:00:00.000Z"
